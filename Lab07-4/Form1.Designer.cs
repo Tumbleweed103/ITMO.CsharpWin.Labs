7980f0415bf0e477024de6fb91a60039a30ba8e0
Tumbleweed103 <antatgm@gmail.com>
2020-12-17 00:39:51 +0300

    -

--- Lab07-4/Form1.Designer.cs
+++ Lab07-4/Form1.Designer.cs
@@ -29,13 +29,116 @@ namespace Lab07_4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 9);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(86, 17);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Значение A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(205, 9);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(86, 17);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Значение B";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(104, 9);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 22);
+            this.txbA.TabIndex = 3;
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(297, 9);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 22);
+            this.txbB.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(15, 42);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(98, 27);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(15, 75);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(98, 27);
+            this.btnWork.TabIndex = 6;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(120, 75);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Разность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(407, 153);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lab07-4/Form1.cs
+++ Lab07-4/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,81 @@ namespace Lab07_4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private delegate int AsyncSumm(int a, int b);
+        delegate void PrintLabel(string str);
+        private PrintLabel PrintDelegateFunc;
+
+        private int Summ(int a, int b)
+        {
+            Thread.Sleep(2000);
+            return a + b;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = int.Parse(txbA.Text);
+                b = int.Parse(txbB.Text);
+                AsyncSumm summDelegate = new AsyncSumm(Summ);
+                AsyncCallback cb = new AsyncCallback(CallBackMethod);
+                summDelegate.BeginInvoke(a, b, cb, summDelegate);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнении преобразования типов произошла ошибка");
+            }
+        }
+
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            string str;
+            AsyncSumm summDelegate = (AsyncSumm)ar.AsyncState;
+            str = String.Format($"Сумма введенных чисел равна {summDelegate.EndInvoke(ar)}");
+            PrintDelegateFunc = new PrintLabel(PrintFunc);
+            lblResult.Invoke(PrintDelegateFunc, new object[] { str });
+            MessageBox.Show(str, "Результат операции");
+        }
+
+        private void btnWork_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа кипит!!!");
+        }
+
+        void PrintFunc(string str)
+        {
+            lblResult.Text = str;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = int.Parse(txbA.Text);
+                b = int.Parse(txbB.Text);
+                int res = await Subb(a, b);
+                lblResult.Text = res.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнении преобразования типов возникла ошибка"); 
+                txbA.Text = txbB.Text = "";
+                return;
+            }
+        }
+
+        private async Task<int> Subb(int a, int b)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return a - b;
+            }
+            );
         }
     }
 }
